18484f1aa89af4db83fb42ee95f733118d500df3
Andreas <andreas.merentitis@gmail.com>
2020-08-30 11:59:39 +0200

    update client

--- client_api_standalone.py
+++ client_api_standalone.py
@@ -15,13 +15,8 @@ import click
 import requests
 import json
 
-
-import pdb
-
-
 @click.command(short_help='Classifies datapoints from json input file.')
-
-def datapoint_classification_client(apollo_opts):
+def datapoint_classification_client():
     """Classifies datapoints from json input file.
 
     Takes one or more datapoints and returns the results from a TensorFlow"""
@@ -38,51 +33,33 @@ def datapoint_classification_client(apollo_opts):
     method = 'POST'
     headers = {'Content-Type': 'application/json'} 
     service = 'execute-api'
-    data1 = 'https://qcqzz1dqtd.execute-api.eu-west-1.amazonaws.com/Test_Carparts47_LSQSL/carparts47-lsqsl'
-    data2 = 'https://xjnv2iidq2.execute-api.eu-west-1.amazonaws.com/API_Carparts47_LTSQSL/carparts47-ltsqsl'
+    url = 'https://93mxgice87.execute-api.eu-west-1.amazonaws.com/dev/infer'
     region = 'eu-west-1'
 
     print('')
-    print(data1)
-    print(data2)
+    print(url)
     print('')
 
     logger.info('Creating request...')
-
+    
     auth = {}
     data = {}
-    data['data_to_classify'] = tuple(points['features'])
+    data['input'] = points['input']
+    data['epoch'] = points['epoch']
     json_data = json.dumps(data)
 
-    json_response1 = requests.request(method, url1, auth=auth, data=json_data, headers=headers)
-    json_response2 = requests.request(method, url2, auth=auth, data=json_data, headers=headers)
+    json_response = requests.request(method, url, auth=auth, data=json_data, headers=headers)
 
     end_time = time.time()
     
     # Extract text from JSON
-    response1 = json.loads(json_response1.text)
-    response2 = json.loads(json_response2.text)
+    response = json.loads(json_response.text)
+    flattened_response = [val for sublist in response for val in sublist]
+    print(response)
 
-    flattened_response1 = [val for sublist in response1 for val in sublist]
-    flattened_response2 = [val for sublist in response2 for val in sublist]
-
-    for d1, d2 in zip(flattened_response1, flattened_response2):
-       if isinstance(d1, str) or isinstance(d2, str):
-          pass
-       else:
-          for key, value in d1.items():
-              if value != d2[key]:
-                 print (key, value, d2[key])
-
-
-    print ('')
-    print(flattened_response1)
-
-    print ('')
-    print(flattened_response2)
     logger.info('End-to-end prediction (including network transfers) of '
                 '{} images took {} seconds.'.format(
-                    len(urls['image_urls']), end_time - start_time))
+                    len(points), end_time - start_time))
 
 
 import logging
@@ -91,7 +68,7 @@ logger.setLevel(logging.INFO)
 
 
 if __name__ == '__main__':
-    image_classification_client()
+    datapoint_classification_client()
 
 
 
