0abae5c6bd0a30fb28e8ab3613d182607461ddb0
Andreas <andreas.merentitis@gmail.com>
2020-09-27 17:33:44 +0200

    update timeout times

--- client_api_inferqueue.py
+++ client_api_inferqueue.py
@@ -32,7 +32,7 @@ def datapoint_classification_client():
     method = 'POST'
     headers = {'Content-Type': 'application/json'} 
     service = 'execute-api'
-    url = 'https://nzgxwyknkf.execute-api.eu-west-1.amazonaws.com/dev/inferqueue'
+    url = 'https://mjqaf8eq08.execute-api.eu-west-1.amazonaws.com/dev/inferqueue'
     region = 'eu-west-1'
 
     print('')
